1432c82d3fbbcfb3d95ba5778c0acfb1ac24259b
Ismael Valentin <ismaelvale@gmail.com>
2022-02-08 15:49:13 -0400

    Final

--- RPS.js
+++ RPS.js
@@ -1,29 +1,35 @@
 let result = 0;
 let score =[];
+
+function computerPlay() {
+    let options = ["rock", "paper", "scissors"];
+    return options[Math.floor(Math.random() * options.length)];
+}; 
+
        function round() {
-            let playerSelection = prompt("Rock, Paper, or Scissors").toLowerCase;
-            let options = ["rock", "paper", "scissors"];
-            let computerSelection = function computerPlay() {
-                return options[Math.floor(Math.random() * options.length)];
-            }; if (playerSelection == "rock" && computerSelection == "paper") {
-                result += 0;
+            //let playerSelection = prompt("Rock, Paper, or Scissors").toLowerCase();
+            let playerSelection = "rock";
+            let computerSelection = computerPlay();
+            if (playerSelection == "rock" && computerSelection == "paper") {
+                result -= 1;
             } else if (playerSelection == "paper" && computerSelection == "rock") {
-                result += 2;
+                result += 1;
             }  else if (playerSelection == "scissors" && computerSelection == "rock") {
-                result += 0;
+                result -= 1;
             }  else if (playerSelection == "rock" && computerSelection == "rock") {
-                result += 1;
-            }  else if (playerSelection == "paper" && computerSelection == "paper") {
-                result += 1;
-            }  else if (playerSelection == "scissors" && computerSelection == "paper") {
-                result += 2;
-            }  else if (playerSelection == "rock" && computerSelection == "scissors") {
-                result += 2;
-            }  else if (playerSelection == "paper" && computerSelection == "scissors") {
                 result += 0;
-            } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+            }  else if (playerSelection == "paper" && computerSelection == "paper") {
+                result += 0;
+            }  else if (playerSelection == "scissors" && computerSelection == "paper") {
                 result += 1;
+            }  else if (playerSelection == "rock" && computerSelection == "scissors") {
+                result += 1;
+            }  else if (playerSelection == "paper" && computerSelection == "scissors") {
+                result -= 1;
+            } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+                result += 0;
             }
+            console.log(`You picked ${playerSelection} and the computer picked ${computerSelection}, so you earned ${result} points.` )
         return result;
         };
         
@@ -32,8 +38,12 @@ let score =[];
                 round();
                 score.push(result);
             }
-        return score.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+        let final = score.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+        if (final > 0) {
+            console.log("You Won!")
+        } else {
+            console.log("You Lost!");
         };
-
+}
 
 console.log(game());
