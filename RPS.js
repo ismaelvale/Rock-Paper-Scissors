b26f6cf971af4cc61f2ef9340a0555ea4effa407
Ismael Valentin <ismaelvale@gmail.com>
2022-02-08 11:27:51 -0400

    All fucntions included and player prompt

--- RPS.js
+++ RPS.js
@@ -1,26 +1,40 @@
-let options = ["Rock", "Paper", "Scissors"];
+let options = ["rock", "paper", "scissors"];
 function computerPlay () {
     return options[Math.floor(Math.random() * options.length)];
 }
 function round (playerSelection, computerSelection){
-    if (playerSelection == "Rock" && computerSelection == "Paper") {
-        return "You Lose! Paper Covers Rock";
-    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-        return "You Win! Paper Covers Rock";
-    }  else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-        return "You Lose! Rock smashes Scissors";
-    }  else if (playerSelection == "Rock" && computerSelection == "Rock") {
-        return "It's a tie!";
-    }  else if (playerSelection == "Paper" && computerSelection == "Paper") {
-        return "It's a tie!";
-    }  else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        return "You Win! Scissors Cut Paper";
-    }  else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        return "You Win! Rock smashes Scissors";
-    }  else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-        return "You Lose! Scissors Cut Paper";
-    } else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
-        return "It's a tie!";
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return 0;
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return 2;
+    }  else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return 0;
+    }  else if (playerSelection == "rock" && computerSelection == "rock") {
+        return 1;
+    }  else if (playerSelection == "paper" && computerSelection == "paper") {
+        return 1;
+    }  else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return 2;
+    }  else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return 2;
+    }  else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return 0;
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return 1;
     }
 }
 console.log(round("Rock", computerPlay()));
+
+
+
+function game(playerSelection, computerSelection, n) {
+    let playerSelection = prompt("Rock, Paper, or Scissors").toLowerCase;
+    let computerSelection = computerPlay();
+    let score = [];
+    for (let i = 0; i < n; i++) {
+        result = round(playerSelection, computerSelection);
+        score.push(result);
+    }
+    return score.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+}
+console.log(game(playerSelection, computerSelection, 5));
