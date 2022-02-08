074c5beb9a268c202808f3bc543af3ef93313a01
Ismael Valentin <ismaelvale@gmail.com>
2022-02-08 12:31:27 -0400

    Almost there, can't seem to get it to calculate result

--- RPS.js
+++ RPS.js
@@ -1,42 +1,39 @@
-let options = ["rock", "paper", "scissors"];
-function computerPlay () {
-    return options[Math.floor(Math.random() * options.length)];
-}
-function round (){
-    let playerSelection = prompt("Rock, Paper, or Scissors").toLowerCase;
-    let computerSelection = computerPlay();
-    if (playerSelection == "rock" && computerSelection == "paper") {
-        return 0;
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return 2;
-    }  else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return 0;
-    }  else if (playerSelection == "rock" && computerSelection == "rock") {
-        return 1;
-    }  else if (playerSelection == "paper" && computerSelection == "paper") {
-        return 1;
-    }  else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return 2;
-    }  else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return 2;
-    }  else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return 0;
-    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return 1;
-    }
-}
-console.log(round("Rock", computerPlay()));
+let result = 0;
+let score =[];
+       function round() {
+            let playerSelection = prompt("Rock, Paper, or Scissors").toLowerCase;
+            let options = ["rock", "paper", "scissors"];
+            let computerSelection = function computerPlay() {
+                return options[Math.floor(Math.random() * options.length)];
+            }; if (playerSelection == "rock" && computerSelection == "paper") {
+                result += 0;
+            } else if (playerSelection == "paper" && computerSelection == "rock") {
+                result += 2;
+            }  else if (playerSelection == "scissors" && computerSelection == "rock") {
+                result += 0;
+            }  else if (playerSelection == "rock" && computerSelection == "rock") {
+                result += 1;
+            }  else if (playerSelection == "paper" && computerSelection == "paper") {
+                result += 1;
+            }  else if (playerSelection == "scissors" && computerSelection == "paper") {
+                result += 2;
+            }  else if (playerSelection == "rock" && computerSelection == "scissors") {
+                result += 2;
+            }  else if (playerSelection == "paper" && computerSelection == "scissors") {
+                result += 0;
+            } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+                result += 1;
+            }
+        return result;
+        };
+        
+        function game() {
+            for (let i = 0; i < 5; i++) {
+                round();
+                score.push(result);
+            }
+        return score.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+        };
 
 
-
-function game() {
-    let playerSelection = prompt("Rock, Paper, or Scissors").toLowerCase;
-    let computerSelection = computerPlay();
-    let score = [];
-    for (let i = 0; i < 5; i++) {
-        result = round(playerSelection, computerSelection);
-        score.push(result);
-    }
-    return score.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
-}
 console.log(game());
